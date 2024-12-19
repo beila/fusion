8fcb8626af5e5f3e79b18e84a649cf0f0fe1141f
Hojin Ghim <hojin@amazon.com>
2024-12-19 16:36:27 +0000

    C library functions

---
+++ src/libc.nim
@@ -0,0 +1,21 @@
+type
+    const_pointer {.importc: "const void *".} = pointer
+
+proc fwrite*(buf: const_pointer, size: csize_t, count: csize_t, stream: File):
+        csize_t {.exportc.} =
+    return count
+
+proc fflush*(stream: File): cint {.exportc.} =
+    return 0.cint
+
+var
+    stdout* {.exportc.}: File
+    stderr* {.exportc.}: File
+
+proc exit*(status: cint) {.exportc, asmNoStackFrame.} =
+    asm """
+    .loop:
+        cli
+        hlt
+        jmp .loop
+    """
