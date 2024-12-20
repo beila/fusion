a2102b127296a089c7207644276841c2e71c5ba5
Hojin Ghim <hojin@amazon.com>
2024-12-20 18:22:56 +0000

    UEFI Bootloader (Part 2) - Handling exceptions

--- src/bootx64.nim
+++ src/bootx64.nim
@@ -4,11 +4,26 @@ import uefi
 
 proc NimMain() {.importc.}
 
+proc UnhandledException*(e: ref Exception) =
+    echo "Unhandled exception: " & e.msg & " [" & $e.name & "]"
+    if e.trace.len > 0:
+        echo "Stack trace:"
+        echo getStackTrace(e)
+    quit()
+
+proc EfiMainInner(imgHandle: EfiHandle, sysTable: ptr EfiSystemTable): EfiStatus =
+    uefi.sysTable = sysTable
+    consoleClear()
+
+    # force an IndexDefect exception
+    let a = [1, 2, 3]
+    let n = 5
+    discard a[n]
+
 proc EfiMain(imgHandle: EfiHandle, sysTable: ptr EfiSystemTable): EfiStatus {.exportc.} =
     NimMain()
-    uefi.sysTable = sysTable
 
-    consoleClear()
-    echo "Hello, world!"
-
-    quit()
+    try:
+        return EfiMainInner(imgHandle, sysTable)
+    except Exception as e:
+        UnhandledException(e)
--- src/libc.nim
+++ src/libc.nim
@@ -1,13 +1,16 @@
 {.used.}
 
 import uefi
+import std/strutils
 
 type
     const_pointer {.importc: "const void *".} = pointer
 
 proc fwrite*(buf: const_pointer, size: csize_t, count: csize_t, stream: File): csize_t {.exportc.} =
     let output = $cast[cstring](buf)
-    consoleOut(output)
+    for line in output.splitLines(keepEOL = true):
+        consoleOut(line)
+        consoleOut("\r")
     return count
 
 proc fflush*(stream: File): cint {.exportc.} =
