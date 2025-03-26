2f4398aa24e152bca71a16b062ff02dff3495402
Hojin Ghim <hojin@amazon.com>
2025-03-26 20:53:01 +0000

    Add OVMF

--- flake.nix
+++ flake.nix
@@ -5,11 +5,18 @@
     };
     outputs={nixpkgs,...}: let
         system = "aarch64-darwin";
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+            inherit system;
+            config = {
+                # Package ‘OVMF-202411’ is marked as broken, refusing to evaluate.
+                allowBroken = true;
+            };
+        };
     in {
         devShells.${system}.simple = pkgs.mkShell {
             packages = with pkgs;
                 [
+                    OVMF
                     just
                     libllvm
                     lld
